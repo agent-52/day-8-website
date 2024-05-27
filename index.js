e6e80c04d50e016c69d5b28a853aed8945b752d5
agent-52 <abhaybhadauriya52@gmail.com>
2024-05-27 17:04:32 +0530

    complete page1

---
+++ index.js
@@ -0,0 +1,32 @@
+gsap.from(".cap",{
+    y:-15,
+    delay:1,
+    duration:1.6,
+    ease: "expoScale(0.5,7,none)",
+})
+gsap.from(".cap1",{
+    y:-15,
+    delay:1,
+    duration:1.6,
+    ease: "expoScale(0.5,7,none)",
+})
+
+gsap.from(".openedBottle",{
+    y:-470,
+    delay:1,
+    duration:1.6,
+    ease: "expoScale(0.5,7,none)",
+})
+gsap.from(".largeCircle",{
+    scale:0.7,
+    delay:1.4,
+    duration:1,
+    ease: "expoScale(0.5,7,none)",
+})
+gsap.from(".page1H,.page1Desc,.shopButton1",{
+    scale:0,
+    opacity:0,
+    delay:1.6,
+    duration:1,
+    ease: "expoScale(0.5,7,none)",
+})
