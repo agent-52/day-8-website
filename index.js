0701ffdc9cab4268a7036a501848e37a3197f718
agent-52 <abhaybhadauriya52@gmail.com>
2024-05-29 16:57:23 +0530

    complete page2 page3 page4 page5 page6 page7

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 gsap.from(".cap",{
     y:-15,
     delay:1,
-    duration:1.6,
+    duration:1.8,
     ease: "expoScale(0.5,7,none)",
 })
 gsap.from(".cap1",{
     y:-15,
     delay:1,
-    duration:1.6,
+    duration:1.8,
     ease: "expoScale(0.5,7,none)",
 })
 
 gsap.from(".openedBottle",{
     y:-470,
     delay:1,
-    duration:1.6,
+    duration:1.8,
     ease: "expoScale(0.5,7,none)",
 })
 gsap.from(".largeCircle",{
     scale:0.7,
-    delay:1.4,
-    duration:1,
+    delay:1.2,
+    duration:1.4,
     ease: "expoScale(0.5,7,none)",
 })
 gsap.from(".page1H,.page1Desc,.shopButton1",{
@@ -30,3 +30,68 @@ gsap.from(".page1H,.page1Desc,.shopButton1",{
     duration:1,
     ease: "expoScale(0.5,7,none)",
 })
+////////////////////////////////////////////////////
+gsap.to(".openedBottle",{
+    yPercent:37.5,
+    delay:1,
+    duration:1.8,
+    ease: "expoScale(0.5,7,none)",
+    scrollTrigger:{
+        trigger:".cap",
+        // markers:true,
+        scrub:1,
+        start:"-200% top",
+        end:"400% top"
+    }
+})
+gsap.to(".cap",{
+    yPercent:999.5,
+    delay:1,
+    duration:1.8,
+    ease: "expoScale(0.5,7,none)",
+    scrollTrigger:{
+        trigger:".cap",
+        // markers:true,
+        scrub:1,
+        start:"-250% top",
+        end:"400% top"
+    }
+})
+gsap.to(".cap1",{
+    yPercent:1225.5,
+    delay:1,
+    duration:1.8,
+    ease: "expoScale(0.5,7,none)",
+    scrollTrigger:{
+        trigger:".cap",
+        // markers:true,
+        scrub:1,
+        start:"-250% top",
+        end:"400% top"
+    }
+})
+
+////////////////////
+
+gsap.fromTo(".openedBottle", { yPercent:37.5 }, { yPercent:133,
+    ease: "expoScale(0.5,7,none)",
+    scrollTrigger:{
+        trigger:".page2",
+        markers:true,
+        scrub:1,
+        start:"4% top",
+        end:"bottom top"
+    }
+
+});
+gsap.fromTo(".cap", { yPercent:999.5 }, { yPercent:1330,
+    ease: "expoScale(0.5,7,none)",
+    scrollTrigger:{
+        trigger:".page2",
+        markers:true,
+        scrub:1,
+        start:"4% top",
+        end:"bottom top"
+    }
+
+});
