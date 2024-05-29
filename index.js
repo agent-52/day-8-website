ebc10a239fbf732ebafb4b49d11ca67385f82192
agent-52 <abhaybhadauriya52@gmail.com>
2024-05-29 17:35:29 +0530

    keep the main image in the main body and remove them from page1 to solve scrolling issues

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 gsap.from(".cap",{
-    y:-15,
+    yPercent:235,
     delay:1,
     duration:1.8,
     ease: "expoScale(0.5,7,none)",
 })
 gsap.from(".cap1",{
-    y:-15,
+    yPercent:225,
     delay:1,
     duration:1.8,
     ease: "expoScale(0.5,7,none)",
@@ -31,67 +31,66 @@ gsap.from(".page1H,.page1Desc,.shopButton1",{
     ease: "expoScale(0.5,7,none)",
 })
 ////////////////////////////////////////////////////
-gsap.to(".openedBottle",{
-    yPercent:37.5,
-    delay:1,
-    duration:1.8,
-    ease: "expoScale(0.5,7,none)",
-    scrollTrigger:{
-        trigger:".cap",
-        // markers:true,
-        scrub:1,
-        start:"-200% top",
-        end:"400% top"
-    }
-})
-gsap.to(".cap",{
-    yPercent:999.5,
-    delay:1,
-    duration:1.8,
-    ease: "expoScale(0.5,7,none)",
-    scrollTrigger:{
-        trigger:".cap",
-        // markers:true,
-        scrub:1,
-        start:"-250% top",
-        end:"400% top"
-    }
-})
-gsap.to(".cap1",{
-    yPercent:1225.5,
-    delay:1,
-    duration:1.8,
-    ease: "expoScale(0.5,7,none)",
-    scrollTrigger:{
-        trigger:".cap",
-        // markers:true,
-        scrub:1,
-        start:"-250% top",
-        end:"400% top"
-    }
-})
+// gsap.to(".openedBottle",{
+//     yPercent:37.5,
+//     delay:1,
+//     duration:1.8,
+//     ease: "expoScale(0.5,7,none)",
+//     scrollTrigger:{
+//         trigger:".cap",
+//         // markers:true,
+//         scrub:1,
+//         start:"-200% top",
+//         end:"400% top"
+//     }
+// })
+// gsap.to(".cap",{
+//     yPercent:999.5,
+//     delay:1,
+//     duration:1.8,
+//     ease: "expoScale(0.5,7,none)",
+//     scrollTrigger:{
+//         trigger:".cap",
+//         // markers:true,
+//         scrub:1,
+//         start:"-250% top",
+//         end:"400% top"
+//     }
+// })
+// gsap.to(".cap1",{
+//     yPercent:1225.5,
+//     delay:1,
+//     duration:1.8,
+//     ease: "expoScale(0.5,7,none)",
+//     scrollTrigger:{
+//         trigger:".cap",
+//         // markers:true,
+//         scrub:1,
+//         start:"-250% top",
+//         end:"400% top"
+//     }
+// })
 
 ////////////////////
 
-gsap.fromTo(".openedBottle", { yPercent:37.5 }, { yPercent:133,
-    ease: "expoScale(0.5,7,none)",
-    scrollTrigger:{
-        trigger:".page2",
-        markers:true,
-        scrub:1,
-        start:"4% top",
-        end:"bottom top"
-    }
+// gsap.fromTo(".openedBottle", { yPercent:37.5 }, { yPercent:133,
+//     ease: "expoScale(0.5,7,none)",
+//     scrollTrigger:{
+//         trigger:".page2",
+//         markers:true,
+//         scrub:1,
+//         start:"4% top",
+//         end:"bottom top"
+//     }
 
-});
-gsap.fromTo(".cap", { yPercent:999.5 }, { yPercent:1330,
-    ease: "expoScale(0.5,7,none)",
-    scrollTrigger:{
-        trigger:".page2",
-        markers:true,
-        scrub:1,
-        start:"4% top",
-        end:"bottom top"
-    }
+// gsap.fromTo(".cap", { yPercent:999.5 }, { yPercent:1330,
+//     ease: "expoScale(0.5,7,none)",
+//     scrollTrigger:{
+//         trigger:".page2",
+//         markers:true,
+//         scrub:1,
+//         start:"4% top",
+//         end:"bottom top"
+//     }
 
-});
+// });
